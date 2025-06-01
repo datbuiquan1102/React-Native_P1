9e55d36f915b09870de38b2a429b2b2c360532e4
datbuiquan1102 <67991427+datbuiquan1102@users.noreply.github.com>
2025-06-01 10:35:45 +0700

    video 32

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -10,48 +10,49 @@ export default function App() {
     { id: 2, title: "Ton Ngo Khong" },
     { id: 3, title: "Duong Tang Tang" },
     { id: 4, title: "Tru Bat Gioi" },
-    { id: 5, title: "Sa Ngo Tinh" },
-    { id: 6, title: "Sa Ngo Tinh" },
-    { id: 7, title: "Sa Ngo Tinh" },
-    { id: 8, title: "Sa Ngo Tinh" },
-    { id: 9, title: "Sa Ngo Tinh" }
+    { id: 5, title: "Sa Ngo Tinh 1" },
+    { id: 6, title: "Sa Ngo Tinh 2" },
+    { id: 7, title: "Sa Ngo Tinh 3" },
+    { id: 8, title: "Sa Ngo Tinh 4" },
+    { id: 9, title: "Sa Ngo Tinh 5" },
+    { id: 10, title: "Sa Ngo Tinh 6" }
   ])
 
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View>
+      <Text style={{ color: 'red', fontSize: 30 }}>{name}</Text>
 
-        <Text style={{ color: 'red', fontSize: 30 }}>{name}</Text>
-
-        <TextInput
-          onChangeText={v => setName(v)}
-          autoCapitalize='none'
-          // keyboardType='numeric'
-          // value={name}
-          style={{
-            borderColor: 'violet',
-            borderWidth: 1,
-            padding: 10,
-          }}>
-        </TextInput>
+      <TextInput
+        onChangeText={v => setName(v)}
+        autoCapitalize='none'
+        // keyboardType='numeric'
+        // value={name}
+        style={{
+          borderColor: 'violet',
+          borderWidth: 1,
+          padding: 10,
+        }}>
+      </TextInput>
 
 
-        <Button title='Add New'
-          color={'green'}
-          onPress={() => alert("ok")}
-        ></Button>
+      <Button title='Add New'
+        color={'green'}
+        onPress={() => alert("ok")}
+      ></Button>
 
-        <View style={{ marginTop: 20, borderColor: 'red', borderWidth: 1 }}>
-          {todoList.map(todo => {
-            return (
-              <Text style={styles.todo}>{todo.title}</Text>
-            )
-          })}
-        </View>
+      <ScrollView style={{
+        marginTop: 20,
+        borderColor: 'red', borderWidth: 1
+      }}>
+        {todoList.map(todo => {
+          return (
+            <Text key={todo.id} style={styles.todo}>{todo.title}</Text>
+          )
+        })}
+      </ScrollView>
 
-      </View>
     </View>
   );
 }
